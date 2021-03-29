16de0ec386e3991fcb5bb6df71632eb3a380eb9e
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-03-28 21:32:29 -0600

    removed helper code

--- employeeTracker.js
+++ employeeTracker.js
@@ -149,67 +149,6 @@ const addDepartment = () => {
         });
 };
 
-
-
-// const bidAuction = () => {
-//     // query the database for all items being auctioned
-//     connection.query('SELECT * FROM auctions', (err, results) => {
-//         if (err) throw err;
-//         // once you have the items, prompt the user for which they'd like to bid on
-//         inquirer
-//             .prompt([{
-//                     name: 'choice',
-//                     type: 'rawlist',
-//                     choices() {
-//                         const choiceArray = [];
-//                         results.forEach(({ item_name }) => {
-//                             choiceArray.push(item_name);
-//                         });
-//                         return choiceArray;
-//                     },
-//                     message: 'What auction would you like to place a bid in?',
-//                 },
-//                 {
-//                     name: 'bid',
-//                     type: 'input',
-//                     message: 'How much would you like to bid?',
-//                 },
-//             ])
-//             .then((answer) => {
-//                 // get the information of the chosen item
-//                 let chosenItem;
-//                 results.forEach((item) => {
-//                     if (item.item_name === answer.choice) {
-//                         chosenItem = item;
-//                     }
-//                 });
-
-//                 // determine if bid was high enough
-//                 if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//                     // bid was high enough, so update db, let the user know, and start over
-//                     connection.query(
-//                         'UPDATE auctions SET ? WHERE ?', [{
-//                                 highest_bid: answer.bid,
-//                             },
-//                             {
-//                                 id: chosenItem.id,
-//                             },
-//                         ],
-//                         (error) => {
-//                             if (error) throw err;
-//                             console.log('Bid placed successfully!');
-//                             start();
-//                         }
-//                     );
-//                 } else {
-//                     // bid wasn't high enough, so apologize and start over
-//                     console.log('Your bid was too low. Try again...');
-//                     start();
-//                 }
-//             });
-//     });
-// };
-
 connection.connect((err) => {
     if (err) throw err;
     start();
